6b163848581fa7ce94d51e0fdb87a645586c3e68
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-10 13:57:28 +0100

    Add project files.

---
+++ Inlämnings uppgift 4.1 Loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace inlämning_._uppgift_._loop_._4_._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x=10;x<31;x++)
+            {
+               Console.WriteLine(x);
+            }
+            {
+                for(int y=200;y>179;y--)
+                {
+                    Console.WriteLine(y);
+                }
+                {
+                    for(int z=1000;z<1401;z=z+50)
+                    {
+                        Console.WriteLine(z);
+                    }
+                }
+            }
+        }
+    }
+}
